3cb9d11edf824bb5d528e2eaa8c2cc68d30a6500
KrhisnaO <kr.oyanedel@duocuc.cl>
2026-04-13 22:57:16 -0400

    NEW

--- src/main/java/com/fullstack3/fullstack3/controller/UsuarioController.java
+++ src/main/java/com/fullstack3/fullstack3/controller/UsuarioController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/usuarios")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:4200")
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
